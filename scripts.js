ed38126351bbfa32f8bff3f0fc7ab516c98a5968
Saul-OG <152898091+Saul-OG@users.noreply.github.com>
2025-08-22 18:32:18 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -7,23 +7,23 @@ document.addEventListener('DOMContentLoaded', () => {
     const texto = input.value.trim();
     if (!texto) return;
 
-    // Crear <li>
+    
     const li = document.createElement('li');
     li.textContent = texto + " ";
 
-    // Crear botón eliminar
+    
     const btnEliminar = document.createElement('button');
     btnEliminar.textContent = "❌";
     btnEliminar.style.marginLeft = "10px";
     btnEliminar.addEventListener('click', () => {
-      li.remove(); // elimina solo este li
+      li.remove(); 
     });
 
-    // Agregar botón al li y li a la lista
+    
     li.appendChild(btnEliminar);
     lista.appendChild(li);
 
-    // Limpiar input
+    
     input.value = '';
     input.focus();
   });
